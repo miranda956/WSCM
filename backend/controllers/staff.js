0e26b8caa27cc4c8339b659a125398bc0e506eb0
miranda956 <musularonald17@gmail.com>
2021-03-30 14:03:36 +0300

    staff api

--- backend/controllers/staff.js
+++ backend/controllers/staff.js
@@ -5,7 +5,7 @@ const express=require("express");
 const db=require("../models");
 function router(app){
 // create staff 
-app.post("staff/api/create",(req,res,next)=>{
+app.post("/api/staff",(req,res,next)=>{
    db.Staff.create({
        f_name:req.body.f_name,
        l_name:req.body.l_name,
@@ -22,6 +22,48 @@ app.post("staff/api/create",(req,res,next)=>{
    })
 
 })
+
+// update staff data 
+app.patch("/api/staff/{id}",(req,res,next)=>{
+    db.staff.update({
+       f_name:req.body.f_name,
+       l_name:req.body.l_name,
+       email:req.body.email,
+       contact:req.body.contact,
+       address:req.body.addresse,
+       position:req.body.position,
+       
+    },{
+        where:{
+            id:req.user.id
+        }
+    }).then((data)=>{
+        res.json(data)
+    }).catch((err)=>{
+        next(err)
+    })
+})
+
+app.get("/api/staff-members",(req,res,next)=>{
+    db.Staff.findAll({}).
+    then((data)=>{
+        res.json(data)
+    }).catch((err)=>{
+        next(err)
+    })
+})
+
+// get single staff member 
+
+app.get("/api/staff/staff-member{id}", (req, res) => {
+  db.Staff.findOne({
+
+  },{
+  where:{
+      id:req.user.id
+  }
+  })
+});
 
 
 
