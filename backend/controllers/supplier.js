568f17532da36a1f082dfc0c971ed62e54d1fdc9
miranda956 <musularonald17@gmail.com>
2021-03-31 14:56:59 +0300

    supplier api

--- backend/controllers/supplier.js
+++ backend/controllers/supplier.js
@@ -5,5 +5,46 @@ const express=require("express");
 const db=require("../models");
 function router(app){
 
+    app.get("/api/supplier-list", (req, res,next) => {
+      db.Supplier.findAll({
+
+      }).then((data)=>{
+        res.json(data)
+      }).catch((err)=>{
+          next(err)
+      })
+    });
+    app.post("/api/supplier", (req, res, next) => {
+      db.Supplier.create({
+          f_name:req.body.f_name,
+          l_name:req.body.l_name,
+          email:req.body.email,
+          contact:req.body.contact,
+          address:req.body.address,
+          pwd:req.body.pwd
+
+      }).then((data)=>{
+         res.json(data)
+      }).catch((err)=>{
+          next(err)
+      })
+    });
+
+    app.patch("/api/supplier/{id}",(req,res,next)=>{
+        db.Supplier.update({
+            f_name:req.body.f_name,
+            l_name:req.body.l_name,
+            email:req.body.email,
+            contact:req.body.contact,
+            address:req.body.address,
+  
+
+        }).then((data)=>{
+            res.json(data)
+
+        }).catch((err)=>{
+            next(err)
+        })
+    })
 }
 module.exports=router;
